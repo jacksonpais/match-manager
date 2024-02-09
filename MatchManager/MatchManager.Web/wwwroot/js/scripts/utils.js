75596c772c1e614df477a33e685fce0ce7f593a2
jacksonpais <jacksonpais@gmail.com>
2024-02-10 01:03:21 +0530

    login register validation and ui

---
+++ MatchManager/MatchManager.Web/wwwroot/js/scripts/utils.js
@@ -0,0 +1,22 @@
+﻿var Utils = function () {
+}
+
+Utils.prototype = {
+    IsNullOrEmpty: function (value) {
+        let rtn = true;
+        if (value !== null) {
+            if (value.trim().length !== 0) {
+                rtn = false;
+            }
+        }
+        return rtn;
+    },
+    IsEmailValid: function(email) {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    }
+}
+var utils = new Utils();
