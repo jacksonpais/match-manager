89c538646624c7f0b1f688b1ab171d6f92a467ee
jacksonpais <jacksonpais@gmail.com>
2024-02-14 15:30:58 +0530

    ui changes for login and registration pages

--- MatchManager/MatchManager.Web/wwwroot/js/scripts/Login/index.js
+++ MatchManager/MatchManager.Web/wwwroot/js/scripts/Login/index.js
@@ -1,6 +1,7 @@
 ﻿var LoginHelper = function () {
     this.form = document.getElementById("login_form");
     this.loginObj = {};
+    this.apiUrl = localStorage.getItem("api_url");
 }
 
 LoginHelper.prototype = {
@@ -14,8 +15,8 @@ LoginHelper.prototype = {
     _processForm: function (e) {
         if (e.preventDefault) e.preventDefault();
         loginHelper.loginObj = {
-            email: document.getElementById("user_email"),
-            password: document.getElementById("user_password")
+            email: document.getElementById("user_email").value,
+            password: document.getElementById("user_password").value
         }
         if (loginHelper._validateForm(loginHelper.loginObj)) {
             loginHelper._submitForm();
@@ -29,17 +30,17 @@ LoginHelper.prototype = {
         let errEmail = document.getElementById("err_user_email");
         let errPassword = document.getElementById("err_user_password");
 
-        if (utils.IsNullOrEmpty(obj.email.value)) {
+        if (utils.IsNullOrEmpty(obj.email)) {
             _displayError(errEmail, true, "Enter Email Id");
-        } else if (!utils.IsEmailValid(obj.email.value)) {
+        } else if (!utils.IsEmailValid(obj.email)) {
             _displayError(errEmail, true, "Enter a valid email");
         } else {
             _displayError(errEmail, false);
         }
 
-        if (utils.IsNullOrEmpty(obj.password.value)) {
+        if (utils.IsNullOrEmpty(obj.password)) {
             _displayError(errPassword, true, "Enter Password");
-        } else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(obj.password.value)) {
+        } else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(obj.password)) {
             _displayError(errPassword, true, "Password must be at least 8 characters long and contain at least 1 letter and 1 number");
         } else {
             _displayError(errPassword, false);
@@ -59,15 +60,28 @@ LoginHelper.prototype = {
         return rtn;
     },
     _submitForm: async function () {
-        const response = fetch(api_url + "account/login", {
+        document.getElementById("successMessage").classList.add("hidden");
+        document.getElementById("errorMessage").classList.add("hidden");
+        const response = fetch(this.apiUrl + "account/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Accept": "application/json",
             },
             body: JSON.stringify(loginHelper.loginObj),
         });
-        const result = await response.json();
-        console.log("Success:", result);
+        const result = await response;
+        let data = await result.json();
+        if (data.isSuccess) {
+            window.location = "/";
+        } else {
+            document.getElementById("errorMessage").classList.remove("hidden");
+            document.getElementById("errorMessage").innerHTML = data.errorMessages[0];
+        }
+    },
+    _clearFields: function () {
+        document.getElementById("user_email").value = "";
+        document.getElementById("user_password").value = "";
     }
 }
 var loginHelper = new LoginHelper();
--- MatchManager/MatchManager.Web/wwwroot/js/scripts/Register/index.js
+++ MatchManager/MatchManager.Web/wwwroot/js/scripts/Register/index.js
@@ -1,6 +1,7 @@
 ﻿var RegisterHelper = function () {
     this.form = document.getElementById("register_form");
     this.registerObj = {};
+    this.apiUrl = localStorage.getItem("api_url");
 }
 
 RegisterHelper.prototype = {
@@ -14,11 +15,11 @@ RegisterHelper.prototype = {
     _processForm: function (e) {
         if (e.preventDefault) e.preventDefault();
         registerHelper.registerObj = {
-            firstName: document.getElementById("user_first_name"),
-            lastName: document.getElementById("user_last_name"),
-            email: document.getElementById("user_email"),
-            password: document.getElementById("user_password"),
-            confirmpassword: document.getElementById("user_confirm_password"),
+            firstName: document.getElementById("user_first_name").value,
+            lastName: document.getElementById("user_last_name").value,
+            email: document.getElementById("user_email").value,
+            password: document.getElementById("user_password").value,
+            confirmpassword: document.getElementById("user_confirm_password").value,
         }
         if (registerHelper._validateForm(registerHelper.registerObj)) {
             registerHelper._submitForm();
@@ -35,41 +36,41 @@ RegisterHelper.prototype = {
         let errPassword = document.getElementById("err_user_password");
         let errConfirmPassword = document.getElementById("err_user_confirm_password");
 
-        if (utils.IsNullOrEmpty(obj.firstName.value)) {
+        if (utils.IsNullOrEmpty(obj.firstName)) {
             _displayError(errFirstName, true, "Enter First Name");
-        } else if (!/^[a-zA-Z ]*$/.test(obj.firstName.value)) {
+        } else if (!/^[a-zA-Z ]*$/.test(obj.firstName)) {
             _displayError(errFirstName, true, "First Name must contain only alphabets");
         } else {
             _displayError(errFirstName, false);
         }
 
-        if (utils.IsNullOrEmpty(obj.lastName.value)) {
+        if (utils.IsNullOrEmpty(obj.lastName)) {
             _displayError(errLastName, true, "Enter Last Name");
-        } else if (!/^[a-zA-Z ]*$/.test(obj.lastName.value)) {
+        } else if (!/^[a-zA-Z ]*$/.test(obj.lastName)) {
             _displayError(errLastName, true, "Last Name must contain only alphabets");
         } else {
             _displayError(errLastName, false);
         }
 
-        if (utils.IsNullOrEmpty(obj.email.value)) {
+        if (utils.IsNullOrEmpty(obj.email)) {
             _displayError(errEmail, true, "Enter Email Id");
-        } else if (!utils.IsEmailValid(obj.email.value)) {
+        } else if (!utils.IsEmailValid(obj.email)) {
             _displayError(errEmail, true, "Enter a valid email");
         } else {
             _displayError(errEmail, false);
         }
 
-        if (utils.IsNullOrEmpty(obj.password.value)) {
+        if (utils.IsNullOrEmpty(obj.password)) {
             _displayError(errPassword, true, "Enter Password");
-        } else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(obj.password.value)) {
+        } else if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(obj.password)) {
             _displayError(errPassword, true, "Password must be at least 8 characters long and contain at least 1 letter and 1 number");
         } else {
             _displayError(errPassword, false);
         }
 
-        if (utils.IsNullOrEmpty(obj.confirmpassword.value)) {
+        if (utils.IsNullOrEmpty(obj.confirmpassword)) {
             _displayError(errConfirmPassword, true, "Enter Confirm Password");
-        } else if (String(obj.password.value) !== String(obj.confirmpassword.value)) {
+        } else if (String(obj.password) !== String(obj.confirmpassword)) {
             _displayError(errConfirmPassword, true, "Confirm Password is not equal to entered password");
         } else {
             _displayError(errConfirmPassword, false);
@@ -89,15 +90,32 @@ RegisterHelper.prototype = {
         return rtn;
     },
     _submitForm: async function () {
-        const response = fetch(api_url + "account/register", {
+        document.getElementById("successMessage").classList.add("hidden");
+        document.getElementById("errorMessage").classList.add("hidden");
+        const response = fetch(this.apiUrl + "account/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Accept": "application/json",
             },
             body: JSON.stringify(registerHelper.registerObj),
         });
-        const result = await response.json();
-        console.log("Success:", result);
+        const result = await response;
+        let data = await result.json();
+        if (data.isSuccess) {
+            document.getElementById("successMessage").classList.remove("hidden");
+            document.getElementById("successMessage").innerHTML = "Almost done! Your account has been created, but in order to proceed, please check your email and click the link inside to confirm your account.";
+        } else {
+            document.getElementById("errorMessage").classList.remove("hidden");
+            document.getElementById("errorMessage").innerHTML = data.errorMessages[0];
+        }
+    },
+    _clearFields: function () {
+        document.getElementById("user_first_name").value = "";
+        document.getElementById("user_last_name").value = "";
+        document.getElementById("user_email").value = "";
+        document.getElementById("user_password").value = "";
+        document.getElementById("user_confirm_password").value = "";
     }
 }
 var registerHelper = new RegisterHelper();
