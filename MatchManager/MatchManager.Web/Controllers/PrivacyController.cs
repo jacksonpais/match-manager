ddd7d0de7d0a66fc52c511694752948973baa64f
jacksonpais <jacksonpais@gmail.com>
2024-03-17 19:28:43 +0530

    privacy policy ui page creation

---
+++ MatchManager/MatchManager.Web/Controllers/PrivacyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatchManager.Web.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        [HttpGet]
+        [Route("privacy-policy")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
