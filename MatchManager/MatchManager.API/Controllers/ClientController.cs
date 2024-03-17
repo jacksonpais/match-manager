c8039a9aebce4967fc4f908eb4c1f11eb70f28e5
jacksonpais <jacksonpais@gmail.com>
2024-03-17 19:26:31 +0530

    Client changes

---
+++ MatchManager/MatchManager.API/Controllers/ClientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+    }
+}
---
+++ MatchManager/MatchManager.Web/Controllers/ClientController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatchManager.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        [HttpGet]
+        [Route("add-clients")]
+        public IActionResult AddClients()
+        {
+            return View();
+        }
+    }
+}
